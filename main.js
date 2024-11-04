b2fe5ed833e3168b8c23e64d0588d6d085070439
Andrew Perotti <Perotti28@gmail.com>
2024-11-04 11:13:59 -0800

    Add pseudo code as a plan to your program
    By writing pseudo code first we avoid a lot of problems, like getting
    lost, or diving down rabbit holes. We need to focus on the problem at
    hand and not over do it. Pseudo code can also help others understand
    what we are doing at each point in our program.

---
+++ main.js
@@ -0,0 +1,11 @@
+//Save the weapon choices to a variable, as lowercase strings
+
+//Select the computers weapon choice randomly, save as a variable 
+
+//Save the user input as their weapon choice in a variable, convert input to lower case
+
+//Define variables that will hold the score of the humanPlayer and the computerPlayer, give them a starting value of zero
+
+//Create a one round play through, using a function to decide who wins and incrementing the correct score variable
+
+//output the winner, via console output or prompt
