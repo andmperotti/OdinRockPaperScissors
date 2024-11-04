2ae2bcb3a76c503049f794dc068112b630bc6110
Andrew Perotti <Perotti28@gmail.com>
2024-11-04 11:31:30 -0800

    Add logic to javascript for a game play through
    Filled in the javascript file with necessary declarations and logic to
    play a single round of the rock, paper, scissors game.

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 //Save the weapon choices to a variable, as lowercase strings
-
+let choices = ['scissors', 'rock', 'paper']
 //Select the computers weapon choice randomly, save as a variable 
-
+let computerWeapon = choices[Math.floor(Math.random()*3)]
 //Save the user input as their weapon choice in a variable, convert input to lower case
-
+let humanWeapon = prompt("What weapon would you like to use? ").toLowerCase()
 //Define variables that will hold the score of the humanPlayer and the computerPlayer, give them a starting value of zero
-
+let humanScore = 0;
+let computerScore = 0
 //Create a one round play through, using a function to decide who wins and incrementing the correct score variable
-
-//output the winner, via console output or prompt
+function playRockPaperScissors(computerWeapon, humanWeapon){
+    if(
+        (computerWeapon==='rock'&&humanWeapon==='scissors')
+        || (computerWeapon==='scissors'&&humanWeapon==='paper')
+        || (computerWeapon==='paper'&&humanWeapon==='rock')
+    ){
+        computerScore++
+        console.log('Computer wins')
+    }else if(computerWeapon===humanWeapon){
+        console.log("Tie, play again!")
+    }else{
+        humanScore++
+        console.log("Human wins!")
+    }
+}
+playRockPaperScissors(computerWeapon, humanWeapon)
