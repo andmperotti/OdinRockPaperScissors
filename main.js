d174ad76bdf1c65a93fc06f6491895f9fb101b3a
Andrew Perotti <Perotti28@gmail.com>
2024-11-04 13:07:12 -0800

    Omit commented out code no longer in use
    Had previously left commented out code, which isn't needed as we
    refactored it into a new function.
    Added comments within that new function to explain steps of it like we
    previously had in the program.

--- main.js
+++ main.js
@@ -1,43 +1,8 @@
-// //Create a function that returns one of 3 choices, and save its return as a variable to symbolized the computers choice
-// function getComputerChoice(){
-//     return ['rock', 'paper', 'scissors'][Math.floor(Math.random()*3)]
-// }
-// let computerWeapon = getComputerChoice()
-
-// //Create a function asks the user what weapon they'd like to use in the game, and save the return to a variable
-// function getHumanChoice(){
-//     return prompt("What weapon would you like to use? ").toLowerCase()
-// }
-// let humanWeapon = getHumanChoice()
-
-// //Define variables that will hold the score of the humanPlayer and the computerPlayer, give them a starting value of zero
-// let humanScore = 0;
-// let computerScore = 0
-
-// //Create a one round play through, using a function, to decide who wins and incrementing the correct score variable
-// function playRound(computerChoice, humanChoice){
-//     if(
-//         (computerChoice==='rock'&&humanChoice==='scissors')
-//         || (computerChoice==='scissors'&&humanChoice==='paper')
-//         || (computerChoice==='paper'&&humanChoice==='rock')
-//     ){
-//         computerScore++
-//         console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-//     }else if(computerChoice===humanChoice){
-//         console.log("Tie, play again!")
-//     }else{
-//         humanScore++
-//         console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-//     }
-// }
-//invoke one play through by invoking play function
-// playRound(computerWeapon, humanWeapon)
-
-
 //Create a function 'playGame' that calls your single round play through 5 times
     //Move previously made variables and functions inside this function, so we avoid global variables,etc
 
 function playGame(){
+    //function for one play through
     function playRound(computerChoice, humanChoice){
         if(
             (computerChoice==='rock'&&humanChoice==='scissors')
@@ -53,15 +18,18 @@ function playGame(){
             console.log(`You win! ${humanChoice} beats ${computerChoice}`)
         }
     }
+    //function for setting computer weapon
     function getComputerChoice(){
         return ['rock', 'paper', 'scissors'][Math.floor(Math.random()*3)]
     }
+    //function for setting user weapon
     function getHumanChoice(){
         return prompt("What weapon would you like to use? ").toLowerCase()
     }
+    //score variables to be used to deem winner of multiple rounds
     let humanScore = 0;
     let computerScore = 0
-
+    //loop to run single round 5 times, which calls both choice functions and checks for winner/loser/tie
     for(let i = 0; i<5; i++){
         let computerWeapon = getComputerChoice()
         let humanWeapon = getHumanChoice()
